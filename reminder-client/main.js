a1a171164fdb96eadab559d75c5e77c925bd8409
Oliver Lau <oliver@ersatzworld.net>
2024-11-21 16:14:37 +0100

    Improved help. Added option to post immediately. Some beautifications.

--- reminder-client/main.js
+++ reminder-client/main.js
@@ -1,6 +1,15 @@
 // Copyright (c) 2024 Heise Medien GmbH & Co. KG, Oliver Lau <ola@ct.de>
 
 (function (window) {
+    const HOSTNAME = window.location.hostname;
+    const PORT = 3421;
+    const MILLISECONDS = 1000;
+    const API = {
+        ENDPOINT: {
+            SCHEDULE: `http://${HOSTNAME}:${PORT}/api/v1/appointment/schedule`,
+            CANCEL: `http://${HOSTNAME}:${PORT}/api/v1/appointment/cancel`,
+        }
+    };
     const LOCALSTORAGE = {
         KEY: {
             TITLE: "teams-reminder-title",
@@ -12,28 +21,23 @@
             REMINDERS: "teams-reminder-reminders",
         },
     };
-    const API = {
-        ENDPOINT: {
-            SCHEDULE: "http://127.0.0.1:3421/api/v1/appointment/schedule",
-            CANCEL: "http://127.0.0.1:3421/api/v1/appointment/cancel",
-        }
-    };
     const CONFIG = {
-        REQUEST_TIMEOUT_MS: 10000,
-        STATUS_TIMEOUT_MS: 2000,
+        REQUEST_TIMEOUT_MS: 10 * MILLISECONDS,
+        STATUS_TIMEOUT_MS: 2 * MILLISECONDS,
         FADEOUT_MS: 300,
     };
-    const MILLISECONDS = 1000;
 
     class TimeSpanSelector extends HTMLElement {
         static observedAttributes = ["reminders", "date-time"];
         static formAssociated = true;
         static DELETE_STRING = "löschen";
+        static IMMEDIATELY = "immediately";
         static UNITS = {
             s: 1,
             m: 60,
             h: 60 * 60,
             d: 24 * 60 * 60,
+            [TimeSpanSelector.IMMEDIATELY]: -1,
         };
         static UNIT_NAMES = {
             s: "Sekunde(n)",
@@ -87,7 +91,7 @@ div {
     font-weight: bold;
 }
 :host {
-    --plus-size: 20px;
+    --plus-size: 22px;
 }
 .plus {
     display: inline-block;
@@ -95,21 +99,16 @@ div {
     height: var(--plus-size);
     border-radius: 50%;
     background-color: transparent;
-    border: calc(var(--plus-size) / 10) solid var(--dark-color);
-    color: var(--dark-color);
-    text-align: center;
     cursor: pointer;
-    position: relative;
 }
-.plus:before{
-    position: absolute;
-    top: -3px;
-    left: 0;
-    content: '\uFF0B';
-    font-size: calc(var(--plus-size) / 1.3);
+.plus > svg {
+    fill: var(--dark-color);
 }
 .plus:hover {
-    background-color: var(--medium-color);
+    background-color: rgb(from var(--medium-color) r g b / 50%);
+}
+.plus:hover > svg {
+    fill: rgb(from var(--dark-color) r g b / 80%);
 }
 `;
             this._shadow.appendChild(style);
@@ -119,6 +118,9 @@ div {
             this._reminders = Object.fromEntries(JSON.parse(this.getAttribute("reminders") || "[]").map(k => [crypto.randomUUID(), k]));
             const plus = document.createElement("div");
             plus.className = "plus";
+            plus.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 11.355 11.355" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5.678,11.355c3.135,0 5.677,-2.543 5.677,-5.677c0,-3.135 -2.543,-5.678 -5.677,-5.678c-3.135,0 -5.678,2.543 -5.678,5.678c-0,3.135 2.543,5.677 5.678,5.677Zm0,-1c-2.584,-0 -4.67,-2.092 -4.67,-4.676c-0,-2.584 2.086,-4.676 4.67,-4.676c2.584,-0 4.676,2.092 4.676,4.676c-0,2.584 -2.092,4.676 -4.676,4.676Zm-2.573,-4.676c0,0.281 0.206,0.474 0.493,0.474l1.593,0l0,1.6c0,0.287 0.2,0.486 0.475,0.486c0.293,0 0.492,-0.199 0.492,-0.486l0,-1.6l1.6,0c0.281,0 0.486,-0.193 0.486,-0.474c0,-0.287 -0.199,-0.492 -0.486,-0.492l-1.6,-0l0,-1.588c0,-0.293 -0.199,-0.493 -0.492,-0.493c-0.275,0 -0.475,0.2 -0.475,0.493l0,1.588l-1.593,-0c-0.293,-0 -0.493,0.205 -0.493,0.492Z" style="fill-rule:nonzero;"/>
+            </svg>`;
             plus.addEventListener("click", _e => {
                 const div = this.addReminder({ n: 1, unit: "m" });
                 this._body.appendChild(div);
@@ -149,34 +151,54 @@ div {
         addReminder(reminder, uuid) {
             if (typeof uuid === "undefined") {
                 uuid = crypto.randomUUID();
-                this._reminders[uuid] = reminder;
             }
+            this._reminders[uuid] = reminder;
             const div = document.createElement("div");
             div.setAttribute("data-uuid", uuid);
             const inputNumber = document.createElement("input");
+            inputNumber.type = "number";
+            inputNumber.min = 0;
+            if (reminder.unit === TimeSpanSelector.IMMEDIATELY) {
+                inputNumber.disabled = true;
+                inputNumber.value = 0;
+            }
+            else {
+                inputNumber.value = reminder.n;
+            }
             inputNumber.addEventListener("change", e => {
                 const uuid = e.target.parentNode.getAttribute("data-uuid");
                 this._reminders[uuid].n = parseInt(e.target.value);
                 this.checkValidity();
                 this._emitChangeEvent();
             });
-            inputNumber.type = "number";
-            inputNumber.value = reminder.n;
             const selectUnit = document.createElement("select");
             selectUnit.addEventListener("change", e => {
                 const uuid = e.target.parentNode.getAttribute("data-uuid");
                 const node = this._shadow.querySelector(`[data-uuid="${uuid}"]`);
-                if (e.target.value === TimeSpanSelector.DELETE_STRING) {
-                    node.remove();
-                    delete this._reminders[uuid];
-                }
-                else {
-                    this._reminders[uuid].unit = e.target.value;
+                switch (e.target.value) {
+                    case TimeSpanSelector.DELETE_STRING:
+                        node.remove();
+                        delete this._reminders[uuid];
+                        break;
+                    case TimeSpanSelector.IMMEDIATELY:
+                        this._reminders[uuid] = { n: 1, unit: TimeSpanSelector.IMMEDIATELY };
+                        inputNumber.disabled = true;
+                        break;
+                    default:
+                        this._reminders[uuid].unit = e.target.value;
+                        inputNumber.disabled = false;
+                        break;
                 }
                 this.checkValidity();
                 this._emitChangeEvent();
             });
+            const optionNow = document.createElement("option");
+            optionNow.value = TimeSpanSelector.IMMEDIATELY;
+            optionNow.textContent = "sofort";
+            selectUnit.appendChild(optionNow);
             for (const [unit, secs] of Object.entries(TimeSpanSelector.UNITS)) {
+                if (unit === TimeSpanSelector.IMMEDIATELY)
+                    continue;
                 const optionUnit = document.createElement("option");
                 optionUnit.value = unit;
                 optionUnit.textContent = TimeSpanSelector.UNIT_NAMES[unit];
@@ -237,6 +259,7 @@ div {
 
     const el = {};
     let abortController = null;
+    let checkInterval;
 
     function showStatus(msg, options = { withProgressBar: false, error: false }) {
         el.form.classList.add("subtle");
@@ -313,8 +336,14 @@ div {
             el.timeSpanSelector.setAttribute("date-time", mkDate(el.date.value, el.startTime.value).toISOString());
     }
 
+    function onKeyUp(e) {
+        if (e.key === "Escape")
+            document.querySelector("#help-floater").classList.add("hidden");
+    }
+
     function onDocumentClicked() {
         hideStatus();
+        document.querySelector("#help-floater").classList.add("hidden");
         if (abortController) {
             abortController.abort();
             abortController = null;
@@ -325,6 +354,7 @@ div {
     function mkAppointmentObject() {
         const t0 = mkDate(el.date.value, el.startTime.value);
         const t1 = mkDate(el.date.value, el.endTime.value);
+        console.debug(el.timeSpanSelector.reminders);
         return {
             "begin_datetime": `${t0.toISOString()}`,
             "end_datetime": `${t1.toISOString()}`,
@@ -450,7 +480,7 @@ div {
         }
         if (todayDateISO === el.date.value) {
             // check for valid start time
-            if (el.startTime.value !== "" && todayTimeISO > el.startTime.value) {
+            if (el.startTime.value !== "" && todayTimeISO >= el.startTime.value) {
                 el.startTime.classList.add("invalid");
                 el.startTime.nextSibling.textContent = "Die Uhrzeit liegt in der Vergangenheit.";
                 ok = false;
@@ -460,7 +490,7 @@ div {
                 el.startTime.nextSibling.textContent = "";
             }
             // check for valid start time
-            if (el.endTime.value !== "" && todayTimeISO > el.endTime.value) {
+            if (el.endTime.value !== "" && todayTimeISO >= el.endTime.value) {
                 el.endTime.classList.add("invalid");
                 el.endTime.nextSibling.textContent = "Die Uhrzeit liegt in der Vergangenheit.";
                 ok = false;
@@ -470,20 +500,18 @@ div {
                 el.endTime.nextSibling.textContent = "";
             }
         }
+        // check if end time is after start time
+        if (el.date.value !== "" && el.startTime.value !== "" && el.endTime.value !== ""
+            && mkDate(el.date.value, el.startTime.value) > mkDate(el.date.value, el.endTime.value)) {
+            el.startTime.classList.add("invalid");
+            el.endTime.classList.add("invalid");
+            el.startTime.nextSibling.textContent = "Start darf nicht nach dem Ende sein.";
+            ok = false;
+        }
         else {
-            // check if end time is after start time
-            if (el.date.value !== "" && el.startTime.value !== "" && el.endTime.value !== ""
-                && mkDate(el.date.value, el.startTime.value) > mkDate(el.date.value, el.endTime.value)) {
-                el.startTime.classList.add("invalid");
-                el.endTime.classList.add("invalid");
-                el.startTime.nextSibling.textContent = "Start darf nicht nach dem Ende sein.";
-                ok = false;
-            }
-            else {
-                el.startTime.classList.remove("invalid");
-                el.endTime.classList.remove("invalid");
-                el.startTime.nextSibling.textContent = "";
-            }
+            el.startTime.classList.remove("invalid");
+            el.endTime.classList.remove("invalid");
+            el.startTime.nextSibling.textContent = "";
         }
         if (
             ok
@@ -546,6 +574,7 @@ div {
         extraStyles.textContent = `:root { --fadeout-ms: ${CONFIG.FADEOUT_MS}ms; }`;
         document.querySelector("head").appendChild(extraStyles);
         document.addEventListener("click", onDocumentClicked);
+        document.addEventListener("keyup", onKeyUp);
         el.timeSpanSelector = document.querySelector("timespan-selector");
         el.timeSpanSelector.addEventListener("change", e => {
             checkForm();
@@ -553,6 +582,33 @@ div {
         });
         restoreForm();
         checkForm();
+
+        document.querySelectorAll(".help").forEach(element => {
+            const helpFloater = document.querySelector("#help-floater");
+            helpFloater.lastChild.addEventListener("click", _e => {
+                helpFloater.classList.add("hidden");
+            });
+            const showHelp = e => {
+                const html = document.querySelector(`#${element.dataset.topic}`).content.cloneNode(true);
+                helpFloater.firstChild.replaceChildren(html);
+                helpFloater.classList.remove("hidden");
+                helpFloater.style.top = `${e.target.offsetTop + 10}px`;
+                helpFloater.style.left = `calc(${e.target.offsetLeft}px + 1.5em + 10px)`;
+            }
+            element.addEventListener("mouseenter", showHelp);
+        });
+
+        window.addEventListener("visibilitychange", e => {
+            if (e.target.visibilityState === "visible") {
+                checkForm();
+                checkInterval = setInterval(checkForm, 5000);
+            }
+            else {
+                clearInterval(checkInterval);
+            }
+        });
+
+        checkInterval = setInterval(checkForm, 5000);
     }
 
     window.addEventListener("load", main);
